3d4173f6334c9e9260b54332189a01ad319a7d11
Julia Terto <julianterto@gmail.com>
2024-11-26 11:36:44 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+using DesafioPOO.Models;
+
+// Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo-01", imei: "1111-KKK", memoria: 64);
+nokia.ExibirInfo();
+nokia.Ligar();
+nokia.InstalarAplicativo("GitHub");
+Console.WriteLine();
+Console.WriteLine("DADOS DO PROXIMO APARELHO:");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "2024PTBR", modelo: "Modelo-Pro", imei: "46656", memoria: 512);
+iphone.ExibirInfo();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Github");
+ 
